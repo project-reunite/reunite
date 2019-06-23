8cf0995865a22012db90a343a8fca75c52293c16
James Cockbain <james.cockbain@ibm.com>
2019-06-23 10:15:43 +0100

    cleaner cypress testing

--- cypress/integration/app.e2e.js
+++ cypress/integration/app.e2e.js
@@ -1,26 +1,38 @@
 describe('e2e app test', () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit('/');
+    cy.clearCookies();
   });
   it('renders welcome panel', () => {
     cy.getElementByDataCy('welcome-panel').should('exist');
     cy.getElementByDataCy('play-button').click();
   });
   it('renders photo-upload panel', () => {
+    cy.getElementByDataCy('play-button').click();
     cy.getElementByDataCy('upload-pic-panel').should('exist');
     cy.getElementByDataCy('no-picture-button').click();
   })
   it('renders selection options', () => {
+    cy.getElementByDataCy('play-button').click();
+    cy.getElementByDataCy('no-picture-button').click();
     cy.getElementByDataCy('gender-selection-card-Male').should('exist');
     cy.getElementByDataCy('gender-selection-card-Female').click()
-    cy.getElementByDataCy('age-selection-card-CHILD').should('exist');
-    cy.getElementByDataCy('age-selection-card-ADULT').click()
+    cy.getElementByDataCy('age-selection-card-ELDERLY').should('exist');
   });
   it('renders 2 cards in a deck', () => {
+    cy.getElementByDataCy('play-button').click();
+    cy.getElementByDataCy('no-picture-button').click();
+    cy.getElementByDataCy('gender-selection-card-Female').click()
+    cy.getElementByDataCy('age-selection-card-ADULT').click()
     cy.getElementByDataCy('person-card').should('exist');
+    cy.getElementByDataCy('deck').should('exist');
     cy.checkNumberOfCardsIs('person-card', 2);
   });
   it('renders the next deck when card is clicked', () => {
+    cy.getElementByDataCy('play-button').click();
+    cy.getElementByDataCy('no-picture-button').click();
+    cy.getElementByDataCy('gender-selection-card-Female').click()
+    cy.getElementByDataCy('age-selection-card-ADULT').click()
     cy.getElementByDataCy('deck').should('exist');
     cy.clickPersonCard('person-card');
     cy.checkNumberOfCardsIs('person-card', 2);
