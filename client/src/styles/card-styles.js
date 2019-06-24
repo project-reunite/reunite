8c3e187f2d746d8a3d0dd2822183ff8b338b02b0
James Cockbain <james.cockbain@ibm.com>
2019-06-24 22:41:55 +0100

    first pass at making the design mobile driven

--- client/src/styles/card-styles.js
+++ client/src/styles/card-styles.js
@@ -1,16 +1,13 @@
 const cardStyle = {
   borderRadius: '20px',
-  // boxShadow: '3px 3px #888888',
 };
 
 const cardFooterStyle = {
   borderRadius: '20px',
-  // boxShadow: '3px 3px #888888',
 };
 
 const cardImageStyle = {
   borderRadius: '20px',
-  // boxShadow: '3px 3px #888888',
 };
 
 module.exports = {
