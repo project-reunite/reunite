e637ec8bef934c176ca278d6b2de6f77d343a57f
James Cockbain <james.cockbain@ibm.com>
2019-06-29 21:05:14 +0100

    add errorMessages module

---
+++ client/src/utils/errorMessages.js
@@ -0,0 +1,7 @@
+const serverError = 'Error receiving information from the server. '
++ 'Please check the server is running. '
++ 'If problem persists, contact a system admin.';
+
+export default {
+  serverError,
+};
