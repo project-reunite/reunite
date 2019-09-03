e79fcdfa1d61a73133fe21e83db08890e9be40ec
James Cockbain <james.cockbain@ibm.com>
2019-09-03 08:00:23 +0100

    updated match card to use hooks

--- client/src/components/cards/match-card/match-card.jsx
+++ client/src/components/cards/match-card/match-card.jsx
@@ -1,12 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import Button from 'mineral-ui/Button';
 import IconSuccess from 'mineral-ui-icons/IconSuccess';
 import IconCancel from 'mineral-ui-icons/IconCancel';
-import Card, {
-  CardImage, CardBlock, CardTitle,
-} from 'mineral-ui/Card';
+import Card, { CardImage, CardBlock, CardTitle } from 'mineral-ui/Card';
 import { FlexItem } from 'mineral-ui/Flex';
 import apiRequests from '../../../utils/apiRequests';
 import ConfirmMatchDialog from '../../dialogs/confirm-match-dialog';
@@ -15,89 +13,74 @@ const { matchCardStyle, cardImageStyle, cardBlockStyle } = require('../../../sty
 const { buttonStyle } = require('../../../styles/button-styles');
 const { iconStyle } = require('../../../styles/icon-styles');
 
-class MatchCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      details: [],
-      isMatchConfirmed: false,
-    };
-  }
+const MatchCard = (props) => {
+  const [details, setDetails] = useState([]);
+  const [isMatchConfirmed, setIsMatchConfirmed] = useState(false);
 
-  componentDidMount = async () => {
-    const { id, onError } = this.props;
-    try {
-      const details = await this.getDetails(id);
-      this.setState({
-        details,
-      });
-    } catch (err) {
-      onError();
+  const { onError, continueSearch, id } = props;
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const personDetails = await apiRequests.getPerson(id);
+        setDetails(personDetails);
+      } catch (err) {
+        onError();
+      }
     }
-  }
+    fetchData();
+  }, [onError, id]);
 
-  getDetails = async personId => apiRequests.getPerson(personId);
-
-  handleClose() {
-    this.setState({
-      isMatchConfirmed: false,
-    });
-  }
-
-  render = () => {
-    const { continueSearch } = this.props;
-    const { details, isMatchConfirmed } = this.state;
-    if (details.data) {
-      const successMessage = `Yes, please reunite me with ${details.data.name}`;
-      const matchCard = (
-        <div>
-          <ConfirmMatchDialog
-            isOpen={isMatchConfirmed}
-            closeDialog={() => this.handleClose()}
-            message="Aid worker contacted!"
-            title="Success"
-          />
-          <div className="cardContainer" data-cy="match-card">
-            <FlexItem>
-              <Card style={matchCardStyle} data-cy="person-card">
-                <CardTitle className="cardTitle">Is this your relative?</CardTitle>
-                <CardImage
-                  style={cardImageStyle}
-                  className="matchCardImage"
-                  src={details.data.img_url}
-                  alt="gradient placeholder"
-                />
-                <CardBlock style={cardBlockStyle}>{`${details.data.name}, ${details.data.age}`}</CardBlock>
-                <CardBlock>
-                  <Button
-                    style={buttonStyle}
-                    iconStart={<IconSuccess style={iconStyle} />}
-                    primary
-                    onClick={() => this.setState({
-                      isMatchConfirmed: true,
-                    })}
-                  >
-                    {successMessage}
-                  </Button>
-                  <Button
-                    style={buttonStyle}
-                    onClick={continueSearch}
-                    iconStart={<IconCancel style={iconStyle} />}
-                  >
-                  No, keep searching
-                  </Button>
-                </CardBlock>
-              </Card>
-            </FlexItem>
-          </div>
-        </div>
-      );
-      return matchCard;
-    }
+  if (!details.data) {
     return null;
-  };
-}
+  }
 
+  const successMessage = `Yes, please reunite me with ${details.data.name}`;
+
+  return (
+    <div>
+      <ConfirmMatchDialog
+        isOpen={isMatchConfirmed}
+        closeDialog={() => setIsMatchConfirmed(false)}
+        message="Aid worker contacted!"
+        title="Success"
+      />
+      <div className="cardContainer" data-cy="match-card">
+        <FlexItem>
+          <Card style={matchCardStyle} data-cy="person-card">
+            <CardTitle className="cardTitle">Is this your relative?</CardTitle>
+            <CardImage
+              style={cardImageStyle}
+              className="matchCardImage"
+              src={details.data.img_url}
+              alt="gradient placeholder"
+            />
+            <CardBlock style={cardBlockStyle}>
+              {`${details.data.name}, ${details.data.age}`}
+            </CardBlock>
+            <CardBlock>
+              <Button
+                style={buttonStyle}
+                iconStart={<IconSuccess style={iconStyle} />}
+                primary
+                onClick={() => setIsMatchConfirmed(true)}
+              >
+                {successMessage}
+              </Button>
+              <Button
+                style={buttonStyle}
+                onClick={continueSearch}
+                iconStart={<IconCancel style={iconStyle} />}
+              >
+                No, keep searching
+              </Button>
+            </CardBlock>
+          </Card>
+        </FlexItem>
+      </div>
+    </div>
+  );
+};
 
 MatchCard.defaultProps = {
   continueSearch: () => {},
