c8a6121691dfac0369f81af48cf37b7539cfcf55
Craig-Forrest <Craig.Forrest@ibm.com>
2019-09-08 15:20:47 +0100

    shuffle 64 images before displaying

--- visualiser-client/src/utils/apiRequests.js
+++ visualiser-client/src/utils/apiRequests.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import config from '../config';
+import { shuffleArray } from './util-functions';
 
 const { origin } = config;
 
 const getPersonUrls = async () => {
   const urls = await axios.get(`${origin}/api/v1/persons/urls`);
+  shuffleArray(urls.data)
   return urls
 };
 
