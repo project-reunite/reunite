b65c7d2c0a0fdd970b9f56cdb665f9be8251e399
Richard Waller <Richard.Waller@ibm.com>
2019-09-08 22:09:13 +0100

    Connect to server; Add text; Shuffle faces consistently; Resolve HTML warnings

--- visualiser-client/src/index.js
+++ visualiser-client/src/index.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import apiRequests from './utils/apiRequests';
+import { shuffleArray } from './utils/util-functions';
+import { origin } from './config';
+import './index.css';
 
 const getUrls = async () => {
   const response = await apiRequests.getPersonUrls();
@@ -9,27 +12,57 @@ const getUrls = async () => {
   return urls;
 }
 
+const getShuffledUrls = async () => {
+  const urls = await getUrls();
+  const shuffledUrls = shuffleArray(urls);
+  return shuffledUrls;
+}
+
 const Page = () => {
   const [urls, setUrls] = useState([]);
-  
-  async function fetchUrls() {
-    try {
-      const urls = await getUrls();
-      setUrls(urls);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  useEffect(() => fetchUrls(), []);
 
-  const faces = urls.map(url => (
-    <Face src={url}></Face>
-  ));
-  return faces;
+  useEffect(() => {
+    async function fetchUrls() {
+      try {
+        const urls = await getShuffledUrls();
+        setUrls(urls);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    fetchUrls()
+  }, []);
+
+  const pageExplanation = <p>
+    Here are 64 missing people. Choose (in your head) who you're looking for
+  </p>;
+
+  const backButton = <button type="button">
+    When you're ready, tap here to go back to the Reunite app and start searching for them
+  </button>;
+
+  const faces = urls.map(url => {
+    const absoluteUrl = `${origin}${url}`;
+    return (<Face key={url} src={absoluteUrl}></Face>);
+  });
+
+  return [
+    pageExplanation,
+    backButton,
+    <br></br>,
+    faces,
+  ];
 }
 
 const Face = (props) => (
-  <img className="face" src={props.src}></img>
+  <img
+    className="face"
+    src={props.src}
+    alt="Missing person"
+  >
+  </img>
+  // For debug:
+  // <p>{props.src}</p>
 )
 
 
--- visualiser-client/src/utils/util-functions.js
+++ visualiser-client/src/utils/util-functions.js
@@ -1,8 +1,14 @@
-function shuffleArray(array) {
+const seedrandom = require('seedrandom');
+
+const rng = seedrandom('aGoodSeed'); // shuffle using the same seed to avoid disorientating users
+
+function shuffleArray(originalArray) {
+    const array = [...originalArray];
     for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
+        const j = Math.floor(rng() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+    return array;
 }
 
 module.exports = {
