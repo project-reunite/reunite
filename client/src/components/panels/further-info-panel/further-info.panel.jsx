076d9840b4277c04a8fd115c4ffbd159452c0b15
Craig-Forrest <Craig.Forrest@ibm.com>
2019-09-10 10:57:55 +0100

    change vote link to make it direct to voting page

--- client/src/components/panels/further-info-panel/further-info.panel.jsx
+++ client/src/components/panels/further-info-panel/further-info.panel.jsx
@@ -46,7 +46,7 @@ const FurtherInfoPanel = () => (
             </a>
           </CardBlock>
           <CardBlock>
-            <a href="https://w3.ibm.com/w3publisher/cognitive-applications/updates-from-bob/a7222250-cf31-11e9-8f65-2d7c559e6321">
+            <a href="https://www.surveygizmo.com/s3/5198505/IBMer-s-Choice-Award-Call-for-Code-The-Internal-Final-Five">
               <Button iconStart={<IconCheckCircle style={iconStyle} />} style={buttonStyle}>
                 <Translate string="further-info.link-to-cfc" />
               </Button>
