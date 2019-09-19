8330fc2a1d7d4a4beef4c29c9efd3bba64ff4e60
James Cockbain <james.cockbain@ibm.com>
2019-09-19 10:13:47 +0100

    adding numbers to missing-people

--- client/src/components/missing-people-screen/missing-people-screen.jsx
+++ client/src/components/missing-people-screen/missing-people-screen.jsx
@@ -1,15 +1,23 @@
-
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import posed, { PoseGroup } from 'react-pose';
 
 import apiRequests from '../../utils/apiRequests';
 import { origin } from '../../config';
 import './missing-people-screen.scss';
 
+const Item = posed.li({
+  flip: {
+    scale: 1,
+    transition: {
+      duration: 2500,
+    },
+  },
+});
+
 const getPersonsInNameOrder = async () => {
   const persons = await apiRequests.getPersonsWithNFeatures();
-  const personsInNameOrder = persons.sort((person1, person2) => (person1.name > person2.name));
-  return personsInNameOrder;
+  return persons.sort((person1, person2) => person1.name > person2.name);
 };
 
 const MissingPeopleScreen = () => {
@@ -30,40 +38,45 @@ const MissingPeopleScreen = () => {
   const numMissingPeople = persons.length || 128;
 
   const pageExplanation = [
-    (
-      <h1>
-        {`Here are the ${numMissingPeople} missing people.`}
-      </h1>
-    ),
-    (
-      <h3>
-        {'Choose (in your head) who you\'re looking for.'}
-      </h3>
-    ),
-    (
-      <h3>
-        {'When you\'re ready, go back to the Reunite app to start searching for them.'}
-      </h3>
-    ),
-    (
-      <p>
-        {'(These photos are generated from celebrity faces. This demo uses these faces because we do not yet have access to a database of refugees)'}
-      </p>
-    ),
+    <h1>{`Here are the ${numMissingPeople} missing people.`}</h1>,
+    <h3>Choose (in your head) who you're looking for.</h3>,
+    <h3>When you're ready, go back to the Reunite app to start searching for them.</h3>,
+    <p>
+      {
+        '(These photos are generated from celebrity faces. This demo uses these faces because we do not yet have access to a database of refugees)'
+      }
+    </p>,
   ];
 
-  const faces = persons.map((person) => {
-    const { name } = person;
-    const url = person.img_url;
-    const absoluteUrl = `${origin}${url}`;
-    return (
-      <Face
-        key={url}
-        src={absoluteUrl}
-        name={name}
-      />
-    );
-  });
+  const faces = (
+    <ul id="#menu">
+      <PoseGroup>
+        {persons.map(person => (
+          <Item key={person.name}>
+            <Face
+              key={person.name}
+              id={person._id}
+              src={`${origin}${person.img_url}`}
+              name={person.name}
+            />
+          </Item>
+        ))}
+      </PoseGroup>
+    </ul>
+  );
+
+  // const faces = persons.map((person) => {
+  //   const { name } = person;
+  //   const url = person.img_url;
+  //   const absoluteUrl = `${origin}${url}`;
+  //   return (
+  //     <Face
+  //       key={url}
+  //       src={absoluteUrl}
+  //       name={name}
+  //     />
+  //   );
+  // });
 
   return (
     <div className="missing-people-screen">
@@ -77,11 +90,10 @@ const MissingPeopleScreen = () => {
 const Face = (props) => {
   const { src, name } = props;
   return (
-    <img
-      className="face"
-      src={src}
-      alt={name}
-    />
+    <div className="person-container">
+      <img className="face" src={src} alt={name} />
+      {name}
+    </div>
   );
 };
 
@@ -89,6 +101,5 @@ Face.propTypes = {
   src: PropTypes.string.isRequired,
   name: PropTypes.number.isRequired,
 };
-
 
 export default MissingPeopleScreen;
