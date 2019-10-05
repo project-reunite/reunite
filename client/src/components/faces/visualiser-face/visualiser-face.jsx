38f128ae8d35bd9e5db1f157fe3b2caeeb376f3a
James Cockbain <james.cockbain@ibm.com>
2019-10-05 11:06:43 +0100

    pass person prop to face

--- client/src/components/faces/visualiser-face/visualiser-face.jsx
+++ client/src/components/faces/visualiser-face/visualiser-face.jsx
@@ -3,49 +3,46 @@ import PropTypes from 'prop-types';
 import FaceChart from '../../charts/face-chart';
 import { AnimatedFaceChartDiv } from '../../animations/div-animations';
 import { generateDataForFaceChart } from '../../../utils/util-functions';
+import { origin } from '../../../config';
 
 const VisualiserFace = (props) => {
   const {
-    src, name, personSeen, id, currentPersons, showFaceCharts, isMobile,
+    person, currentPersons, showFaceCharts, isMobile,
   } = props;
 
   const faceSize = isMobile ? 100 : 200;
 
   let imgClass = 'face';
 
-  if (personSeen) { imgClass += ' filtered'; }
+  if (person.personSeen) { imgClass += ' filtered'; }
 
-  if (currentPersons && currentPersons.includes(id)) { imgClass += ' selected'; }
+  if (currentPersons && currentPersons.includes(person._id)) { imgClass += ' selected'; }
 
   if (showFaceCharts) { imgClass += ' background-image'; }
 
   const faceChart = (
     <AnimatedFaceChartDiv className="person-chart" pose={showFaceCharts ? 'visible' : 'hidden'}>
-      <FaceChart data={generateDataForFaceChart(id)} size={faceSize} />
+      <FaceChart data={generateDataForFaceChart(person._id)} size={faceSize} />
     </AnimatedFaceChartDiv>
   );
 
   return (
     <div className="person-container">
       {faceChart}
-      <img className={imgClass} src={src} alt="Missing person" />
-      {name}
+      <img className={imgClass} src={`${origin}${person.img_url}`} alt="Missing person" />
+      {person.name}
     </div>
   );
 };
 
 VisualiserFace.defaultProps = {
-  personSeen: false,
   isMobile: false,
   currentPersons: [],
 };
 
 VisualiserFace.propTypes = {
-  src: PropTypes.string.isRequired,
-  name: PropTypes.number.isRequired,
-  personSeen: PropTypes.bool,
-  id: PropTypes.string.isRequired,
   isMobile: PropTypes.bool,
+  person: PropTypes.objectOf(PropTypes.any).isRequired,
   currentPersons: PropTypes.arrayOf(PropTypes.string),
   showFaceCharts: PropTypes.bool.isRequired,
 };
