d09244b8fed84bbded66a71ea682a5120e4f07a0
James Cockbain <james.cockbain@ibm.com>
2019-10-07 11:55:44 +0100

    add cross and gradient based on position

--- client/src/components/faces/visualiser-face/visualiser-face.jsx
+++ client/src/components/faces/visualiser-face/visualiser-face.jsx
@@ -7,14 +7,12 @@ import { origin } from '../../../config';
 
 const VisualiserFace = (props) => {
   const {
-    person, currentPersons, showFaceCharts, isMobile,
+    person, currentPersons, showFaceCharts, isMobile, position,
   } = props;
 
   const faceChartRadius = isMobile ? 50 : 100;
 
   let imgClass = 'face';
-
-  if (person.personSeen) { imgClass += ' filtered'; }
 
   if (currentPersons && currentPersons.includes(person._id)) { imgClass += ' selected'; }
 
@@ -26,10 +24,20 @@ const VisualiserFace = (props) => {
     </AnimatedFaceChartDiv>
   );
 
+  const personSeenImage = person.personSeen
+    ? <img className="person-seen-image" src="cross.svg" alt="Missing person" />
+    : null;
+
+  const personImageStyle = position
+    ? { WebkitFilter: 'greyscale(100%)', filter: `grayscale(${100 * position / 128}%)` }
+    : {};
+
+
   return (
     <div className="person-container">
+      {personSeenImage}
       {faceChart}
-      <img className={imgClass} src={`${origin}${person.img_url}`} alt="Missing person" />
+      <img style={personImageStyle} className={imgClass} src={`${origin}${person.img_url}`} alt="Missing person" />
       {person.name}
     </div>
   );
@@ -38,6 +46,7 @@ const VisualiserFace = (props) => {
 VisualiserFace.defaultProps = {
   isMobile: false,
   currentPersons: [],
+  position: 0,
 };
 
 VisualiserFace.propTypes = {
@@ -45,6 +54,7 @@ VisualiserFace.propTypes = {
   person: PropTypes.objectOf(PropTypes.any).isRequired,
   currentPersons: PropTypes.arrayOf(PropTypes.string),
   showFaceCharts: PropTypes.bool.isRequired,
+  position: PropTypes.number,
 };
 
 export default VisualiserFace;
